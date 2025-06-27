383232cde17774ccbcd830a671b17b5beffe5ea5
Denis Khaziev <khaziev.da@gmail.com>
2025-06-27 10:30:16 +0300

    project_done_for_started_css_is_working

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: [
+        "./index.html",
+        "./resources/**/*.{vue,js,ts,jsx,tsx,blade.php}",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-            'vue': 'vue/dist/vue.esm-bundler.js',
-        },
-    },
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-    },
-});
- 
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': '/resources/js',
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
+  server: {
+    hmr: {
+      host: 'localhost'
+    }
+  }
+})

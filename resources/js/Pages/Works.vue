605fd8d29dc552b2e4e9f0a7be6d1dd2442c395c
Denis Khaziev <khaziev.da@gmail.com>
2025-07-28 10:45:38 +0300

    fix before phpstorm

---
+++ resources/js/Pages/Works.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ProductList from '../Components/ProductList.vue'
+import MainPage from '../Components/MainPage.vue'
+
+const props = defineProps({
+  message: String
+})
+</script> 
+<template>
+  <div class="text-center">
+    <MainPage />
+  </div>
+</template>
+
--- telegram-bot/handlers/menu.js
+++ telegram-bot/handlers/menu.js
@@ -4,8 +4,8 @@ export default function (bot, msg) {
     bot.sendMessage(chatId, 'Здравствуйте! Я бот студии красоты Organic Beauty Studio 🌿\nВыберите действие:', {
       reply_markup: {
         inline_keyboard: [
-          [{ text: 'Главная страница', web_app: { url: 'https://1bbf-83-172-153-71.ngrok-free.app/' } }],
-          [{ text: 'Наши работы', web_app: { url: 'https://yourdomain.com/miniapp/works' } }],
+          [{ text: 'Главная страница', web_app: { url: 'https://48af7c834842.ngrok-free.app/' } }],
+          [{ text: 'Наши работы', web_app: { url: 'https://48af7c834842.ngrok-free.app/works' } }],
           [{ text: 'О нас', callback_data: 'about' }]
         ]
       }
